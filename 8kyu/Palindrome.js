9b2e5665d7ccf0db99d833f9f39a327f0367b980
unknown <nateware777@gmail.com>
2024-07-01 12:36:21 +0300

    completed codewars challenge

--- 8kyu/Palindrome.js
+++ 8kyu/Palindrome.js
@@ -3,14 +3,15 @@
 // A palindrome is a word, number, phrase, or other sequence of symbols that reads the same backwards as forwards, such as madam or racecar.
 
 
+//My solutions
 
-
-// My Soultions
 const isPalindrome = (x) => {
     return x.split("").reverse().join("").toLowerCase() === x.toLowerCase() ? true : false
 }
 
 
+
 function isPalindrome(x) {
-    return x.toLowerCase().split('').reverse().join('') == x.toLowerCase()
+    x = x.toLowerCase()
+    return x === x.split('').reverse().join('');
 }
