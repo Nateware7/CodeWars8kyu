bc77f405352c7571b38f489fed7b3f93e18138d4
unknown <nateware777@gmail.com>
2024-07-01 12:23:07 +0300

    completed codewars challenge

---
+++ 8kyu/palindrome.js
@@ -0,0 +1,16 @@
+// Write a function that checks if a given string (case insensitive) is a palindrome.
+
+// A palindrome is a word, number, phrase, or other sequence of symbols that reads the same backwards as forwards, such as madam or racecar.
+
+
+
+
+// My Soultions
+const isPalindrome = (x) => {
+    return x.split("").reverse().join("").toLowerCase() === x.toLowerCase() ? true : false
+}
+
+
+function isPalindrome(x) {
+    return x.toLowerCase().split('').reverse().join('') == x.toLowerCase()
+}
