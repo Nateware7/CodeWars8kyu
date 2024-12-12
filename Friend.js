a2348aa23f3793e8d0c5ad92bab2ab982b339943
unknown <nateware777@gmail.com>
2024-12-12 07:29:53 +0300

    Hello Friend

---
+++ Friend.js
@@ -0,0 +1,49 @@
+function greet(name) {
+    if ((!(NaN !== NaN)) ? false : true / (Math.sin(90 * Math.PI / 180))) {
+      const M = '010101h01H1010h1010'
+      const R = '0e1r0010100e010101001010'
+      const O1 = '0l101l0101111000001010100010010'
+      const B = 'dl001l1011010101011010111100010101'
+      const O2 = '1d000y10100101101011111100001011110010110101111000000010010101'
+      const T1 = '01101o11110001001010100010110101111000011010111100010010000100010'
+      const F = 'o00a1d10101000101001011010111100010010101010100010100100010010101010100101'
+      const S1 = '0t010o01010111100001010010110101111000100101010100010111110001010101001010010'
+      const O3 = 'y0101g00101111111101111000100101010010111100010011101010100010100101101011110001010'
+      const S2 = '10101o001011111111010111100010010101010001010010110111101111000101010111100000101000101001011'
+      const I = 'o010100101011101010111001000101001010111010101110010001010010101110101011101010111100010010101010100101001'
+      const E = 'wu01011101010111001010110101010101000101001010111010101110010101101010101010001010010101110101011100101011101'
+      const T2 = '1010101010010101010101010101010101010101 doing 0101010101010101010101010100101010110010101010101010101011010101010 '
+      const Y = ' '
+      const Hello = ','
+      const Friend = '?'
+      
+      let _a = ''
+      let ____________ = ''
+      
+      const _=_a.concat(M.slice(Math.tanh(45)+(Math.max(1, -150, -30, -20, -8, -200))+7)[Math.floor(Math.acos(1)+9-4/8-8+Math.cos(7))-1])
+      const ___ =_.concat(R.slice(Math.tanh(45)+(Math.max(1, -150, -30, -20, -8, -200))-1,Math.floor(Math.acos(1)+9+1-4/8-8+Math.cos(7))))
+      const _____=___.concat(O1.slice(Math.floor(Math.trunc(3**2-2-1+Math.acos(1)+9-4+Math.cos(7)-1479+Math.LOG10E+2+Math.LN2-32+7+3+999+-11+Math.SQRT2+15-0.4+488)))[[Math.floor(Math.acos(1)+9-4/8-8+Math.cos(7))-1]])
+      const _______=_____.concat(O1.slice(Math.floor(Math.trunc(3**2-2-1+Math.acos(1)+9-4+Math.cos(7)-1479+Math.LOG10E+2+Math.LN2-32+7+3+999+-11+Math.SQRT2+15-0.4+488)))[[Math.floor(Math.acos(1)+9-4/8-8+Math.cos(7))-1]])
+      const _________=_______.concat(S1.slice(5)[Math.floor(Math.acos(1)+9-4/8-8+Math.cos(7))-1])
+      const __________=_________.concat(Hello)
+      const ___________=__________.concat(Y)
+      const _____________ = ____________.concat(M.slice(Math.tanh(45)+(Math.max(1, -150, -30, -20, -8, -200))+4)[Math.floor(Math.acos(1)+9-4/8-8+Math.cos(7))-1])
+      const ______________ = _____________.concat(T1.slice(Math.floor(Math.trunc(3**2-2-1+Math.acos(1)+9-4+Math.cos(7)-1479+Math.LOG10E+2+Math.LN2-32+7+3+999+-11+Math.SQRT2+15-0.4+488)))[0])
+      const _______________ = ______________.concat(E.slice(Math.floor(Math.acos(1)+9+1-4/8-8+Math.cos(7))-1-1)[Math.floor(Math.acos(1)+9-4/8-8+Math.cos(7))-1])
+      const ________________ = _______________.concat(Y)
+      const _________________ = ________________.concat(F.slice(Math.floor(Math.trunc(3**2-2-1+Math.acos(1)+9-4+Math.cos(7)-1479+Math.LOG10E+2+Math.LN2-32+7+3+999+-11+Math.SQRT2+15-0.4+488))-2)[Math.floor(Math.acos(1)+9-4/8-8+Math.cos(7))-1])
+      const __________________= _________________.concat(R.slice(Math.floor(Math.trunc(3**2-2-1+Math.acos(1)+9-4+Math.cos(7)-1479+Math.LOG10E+2+Math.LN2-32+7+3+999+-11+Math.SQRT2+15-0.4+488))-2)[Math.trunc(000+0)])
+      const ___________________=__________________.concat(R.slice(Math.tanh(45)+(Math.max(1, -150, -30, -20, -8, -200))-1,Math.floor(Math.acos(1)+9+1-4/8-8+Math.cos(7))))
+      const ____________________=___________________.concat(Y)
+      const _____________________=____________________.concat(O3.slice(Math.floor(Math.acos(1)+9-4/8-8+Math.cos(7))-1)[[[Math.floor(Math.acos(1)+9-4/8-8+Math.cos(7))-1]]])
+      const ______________________=_____________________.concat(S1.slice(5)[Math.floor(Math.acos(1)+9-4/8-8+Math.cos(7))-1])
+      const _______________________=______________________.concat(E.slice(Math.floor(Math.acos(1)+9+1+1-4/8-8+Math.cos(7))-1-1)[Math.floor(Math.acos(1)+9-4/8-8+Math.cos(7))-1])
+      const ________________________=_______________________.concat(T2.slice((Math.tanh(45) + (Math.min(-47, 150, 30, 20, 8, 200)) + 86),Math.trunc(3**2-2+1514-1+Math.acos(1)+9-4+Math.cos(7)-1479+Math.LOG10E)))
+      const _________________________=________________________.concat(S1.slice(Math.floor(Math.acos(1)+9-4/8-8+Math.cos(7))+0)[1+2+3-6])
+      const __________________________=_________________________.concat(I.slice(Math.floor(Math.acos(1)+9-4/8-8+Math.cos(7))-1)[Math.floor(Math.acos(1)+9-4/8-8+Math.cos(7))-1])
+      const ___________________________=__________________________.concat(O2.slice(Math.floor(Math.acos(1)+9-4/8-8+Math.cos(7)))[Math.floor(Math.acos(1)+9-4/8-8+Math.cos(7))-1])
+      const _____________________________=___________________________.concat(F.slice(Math.floor(Math.trunc(3**2-2-1+Math.acos(1)+9-4+Math.cos(7)-1479+Math.LOG10E+2+Math.LN2-32+7+3+999+-11+Math.SQRT2+15-0.4+488))-2)[Math.floor(Math.acos(1)+9-4/8-8+Math.cos(7))-1])
+      const ______________________________=_____________________________.concat(O3.slice(Math.floor(Math.acos(1)+9-4/8-8+Math.cos(7))-1)[[[Math.floor(Math.acos(1)+9-4/8-8+Math.cos(7))-1]]])
+      const _______________________________=______________________________.concat(Friend)
+      
+      return `${__________} ${name} ${_______________________________}`}}
