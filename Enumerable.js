198bfebfc06478fa9e699e5b9e1dbc6262af120e
unknown <nateware777@gmail.com>
2025-03-15 03:25:19 +0300

    magic

---
+++ Enumerable.js
@@ -0,0 +1,17 @@
+// Description:
+// Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.
+
+
+
+function take(arr, n) {
+   return arr.slice(0, n);
+}
+
+
+const take = (a, n) => a.slice(0, n);
+
+
+function take(arr, n) {
+
+    return  arr.length<n ? arr.slice(): arr.slice(0,n)
+}
