2e65d2cf47a6e02862883bc56c3b3c89a39862e9
unknown <nateware777@gmail.com>
2025-06-02 18:59:03 +0300

    BoToSt

---
+++ toStr.js
@@ -0,0 +1,26 @@
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+
+
+function booleanToString(b){
+    return b.toString()
+  }
+
+function booleanToString(b){
+  return b ? 'true' : 'false'
+}
+
+function booleanToString(b){
+  return String(b)
+}
+
+
+function booleanToString(b){
+  return `${b}`
+}
+
+function booleanToString(b){
+  return b+''
+}
